e95dce898bee012034d56e52b458e6b443613d06
Mirko <github@teamirko.ch>
2020-07-28 14:25:32 +0100

    Add influxdb

--- install.sh
+++ install.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get upgrade
+sudo apt install -y  mosquitto-clients
 sudo apt-get install pythonsudo apt-get install libusb-1.0-0 libudev0 pm-utils
 wget https://download.tinkerforge.com/tools/brickd/linux/brickd_linux_latest_armhf.deb
 sudo dpkg -i brickd_linux_latest_armhf.deb3-pip
-sudo apt install brickd
+
 wget https://download.tinkerforge.com/apt/$(lsb_release -is | tr [A-Z] [a-z])/archive.key -q -O - | sudo apt-key add -
 sudo sh -c "echo 'deb https://download.tinkerforge.com/apt/$(lsb_release -is | tr [A-Z] [a-z]) $(lsb_release -cs) main' > /etc/apt/sources.list.d/tinkerforge.list"
 sudo apt update
+sudo apt install brickd
 sudo apt install python3-tinkerforge
 sudo apt install tinkerforge-mqtt
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo apt-get install -y libffi-dev libssl-dev
+sudo apt-get install -y python3 python3-pip
+sudo apt-get remove python-configparser
+sudo pip3 -v install docker-compose
+sudo usermod -aG docker pi
