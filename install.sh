65def36e8da2ad3cdae0acd0002a39fb5c2466ed
pihome32 <39563875+pihome32@users.noreply.github.com>
2021-04-14 08:12:38 +0200

    Update install.sh
    remove docker

--- install.sh
+++ install.sh
@@ -13,18 +13,18 @@ sudo apt install tinkerforge-mqtt
 bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
 sudo systemctl enable nodered.service
 sudo systemctl start nodered.service
-echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+<<<echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 sudo apt-get update
 sudo apt-get install -y grafana
 sudo /bin/systemctl enable grafana-server
-sudo /bin/systemctl start grafana-server
+sudo /bin/systemctl start grafana-server>>>
 
 sudo apt install python3-tinkerforge
 
-curl -fsSL https://get.docker.com -o get-docker.sh
+<<<curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo apt-get install -y libffi-dev libssl-dev
 sudo apt-get install -y python3 python3-pip
 sudo apt-get remove python-configparser
 sudo pip3 -v install docker-compose
-sudo usermod -aG docker pi
+sudo usermod -aG docker pi>>>
