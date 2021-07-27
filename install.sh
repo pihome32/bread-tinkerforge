34aee33f8f11ca47af3e822ca9866dff385cf35e
pihome <github@teamirko.ch>
2021-07-27 20:47:47 +0100

    remove particle

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 
 sudo apt install -y  mosquitto-clients
 sudo apt-get install pythonsudo apt-get install libusb-1.0-0 libudev0 pm-utils
-wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+
 wget https://download.tinkerforge.com/apt/$(lsb_release -is | tr [A-Z] [a-z])/archive.key -q -O - | sudo apt-key add -
 sudo sh -c "echo 'deb https://download.tinkerforge.com/apt/$(lsb_release -is | tr [A-Z] [a-z]) $(lsb_release -cs) main' > /etc/apt/sources.list.d/tinkerforge.list"
 sudo apt-get update
@@ -13,21 +13,16 @@ sudo apt install tinkerforge-mqtt
 bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
 sudo systemctl enable nodered.service
 sudo systemctl start nodered.service
-<<<echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
-sudo apt-get update
-sudo apt-get install -y grafana
-sudo /bin/systemctl enable grafana-server
-sudo /bin/systemctl start grafana-server>>>
+
 
 sudo apt install python3-tinkerforge
 
-<<<curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+
+
 sudo apt-get install -y libffi-dev libssl-dev
 sudo apt-get install -y python3 python3-pip
 sudo apt-get remove python-configparser
-sudo pip3 -v install docker-compose
-sudo usermod -aG docker pi>>>
+
 sudo systemctl disable hciuart.service
 sudo systemctl disable bluealsa.service
 sudo systemctl disable bluetooth.service
